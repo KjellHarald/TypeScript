d5fb6f417485685902d57e6dca015cfbf1f1eb1c
Kjell Harald <kjell.kodehode@gmail.com>
2022-12-02 14:17:59 +0100

    cleanup

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import about from './about'
+import projects from './projectData'
+
+const RenderProject = ():any => {
+    const proj:any = Object.entries(projects).map((val) => console.log(val))
+    return (
+        <div className='project'>
+            {proj}
+        </div>
+    )
+}
 
 const Render = ():any => {
     const info:any = Object.entries(about).map(([key, val])=> (<p>{key.toLocaleUpperCase()} : {val}</p>))
@@ -7,6 +17,9 @@ const Render = ():any => {
         <div className='TypeScript'>
             <h1>Let's Learn TypeScript!</h1>
             {info}
+            <div className='projects'>
+                <RenderProject />
+            </div>
         </div>
     )
 }
---
+++ src/ts/projectData.ts
@@ -0,0 +1,14 @@
+const projects = [
+    {
+        name: "VersaDB",
+        website: "https://versa-db.com/",
+        description: "A Document-oriented database system written in Python."
+    },
+    {
+        name: "KDH",
+        website: "empty string",
+        description: "empty string"
+    }
+]
+
+export default projects
