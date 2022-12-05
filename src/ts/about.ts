b8431ee2f4a8b2b62dbb238e96c3bde3ccc13a04
Kjell Harald <kjell.kodehode@gmail.com>
2022-12-05 10:31:27 +0100

    @

--- src/ts/about.ts
+++ src/ts/about.ts
@@ -3,16 +3,4 @@ let about:readonly [string, number, string] = [
     new Date().getFullYear() % 1991, 
     "Made in React, with TypeScript."
 ]
-
-/*
-const about: {
-    name: string, 
-    age: number,
-    madeIn: string
-    } = {
-    name: "Kjell Harald Pedersen",
-    age: new Date().getFullYear() % 1991,
-    madeIn: "Made in React, with TypeScript."
-}
-*/
 export default about
