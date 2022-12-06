58d6890dc28a1724b841b837694d95d04a3ccd9a
Kjell Harald <kjell.kodehode@gmail.com>
2022-12-06 09:39:13 +0100

    add color object

---
+++ src/srv/langColors.ts
@@ -0,0 +1,13 @@
+const languageColors: {
+    language:string, 
+    color:string}[] 
+= [
+    {language: "TypeScript", color: "#3178c6"},
+    {language: "Python", color: "#3572A5"},
+    {language: "JavaScript", color: "#f1e05a"},
+    {language: "PHP", color: "#4F5D95"},
+    {language: "R", color: "#198CE7"},
+    {language: "HTML", color: "#e34c26"},
+    {language: "CSS", color: "#563d7c"}
+]
+export default languageColors
