d7f998cc320b6da97397e83b1ed83fdec70b843d
Kjell Harald <kjell.kodehode@gmail.com>
2022-12-08 10:55:09 +0100

    finish contact

--- src/ts/prog/email.tsx
+++ src/ts/prog/email.tsx
@@ -5,25 +5,12 @@ const Email = ():JSX.Element => {
     const [formInfo, setFormInfo] = useState({name:"", email: "", msg: ""})
     const [emailResponse, setEmailResponse] = useState("")
     
-    const updateName = (value:any):any => {
+    const updateForm = (event:any):any => {
         setFormInfo({
             ...formInfo,
-            name: value
+            [event.target.name]: event.target.value
         })
     }
-    const updateEmail = (value:any):any => {
-        setFormInfo({
-            ...formInfo,
-            email: value
-        })
-    }
-    const updateMsg = (value:any):any => {
-        setFormInfo({
-            ...formInfo,
-            msg: value
-        })
-    }
-    
 
     const deliverMsg = async () => {
         const response = await fetch('https://khps.no/mail/', {
@@ -33,14 +20,16 @@ const Email = ():JSX.Element => {
 
         const responseMessage = await response.text()
         setEmailResponse(responseMessage)
+        setFormInfo({name:"", email: "", msg: ""})
     }
+
     return (
         <div className='contact'>
             <h2>Contact</h2>
-            <input onChange={(event)=>updateName(event.target.value)} placeholder='Your name'/>
-            <input onChange={(event)=>updateEmail(event.target.value)} placeholder='Email'/>
-            <textarea onChange={(event)=>updateMsg(event.target.value)} placeholder='Message' />
-            <button onClick={deliverMsg}>Send</button>
+            <input onChange={(event)=>updateForm(event)} name="name" value={formInfo.name} placeholder='Your name'/>
+            <input onChange={(event)=>updateForm(event)} name="email" value={formInfo.email} placeholder='Email'/>
+            <textarea onChange={(event)=>updateForm(event)} name="msg" value={formInfo.msg} placeholder='Message' />
+            {emailResponse !== "" ? <button onClick={deliverMsg} disabled>Send</button> : (<button onClick={deliverMsg}>Send</button>)}
             <p>{emailResponse}</p>
         </div>
     )
