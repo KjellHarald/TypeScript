f550df2313663576441dd561b616e3a1c0aa9a17
Kjell Harald <kjell.kodehode@gmail.com>
2022-12-08 09:52:22 +0100

    update contact form

--- src/ts/prog/email.tsx
+++ src/ts/prog/email.tsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 //import connectVersa from '../../srv/vdb'
 
 const Email = ():JSX.Element => {
+    const [formInfo, setFormInfo] = useState({name:"", email: "", msg: ""})
+    const [emailResponse, setEmailResponse] = useState("")
+    
+    const updateName = (value:any):any => {
+        setFormInfo({
+            ...formInfo,
+            name: value
+        })
+    }
+    const updateEmail = (value:any):any => {
+        setFormInfo({
+            ...formInfo,
+            email: value
+        })
+    }
+    const updateMsg = (value:any):any => {
+        setFormInfo({
+            ...formInfo,
+            msg: value
+        })
+    }
+    
+
+    const deliverMsg = async () => {
+        const response = await fetch('https://khps.no/mail/', {
+            "method": "POST",
+            body: JSON.stringify(formInfo)
+        })
+
+        const responseMessage = await response.text()
+        setEmailResponse(responseMessage)
+    }
     return (
         <div className='contact'>
             <h2>Contact</h2>
-            <input placeholder='Your name'/>
-            <input placeholder='Email'/>
-            <textarea placeholder='Message' />
-            <button>Send</button>
+            <input onChange={(event)=>updateName(event.target.value)} placeholder='Your name'/>
+            <input onChange={(event)=>updateEmail(event.target.value)} placeholder='Email'/>
+            <textarea onChange={(event)=>updateMsg(event.target.value)} placeholder='Message' />
+            <button onClick={deliverMsg}>Send</button>
+            <p>{emailResponse}</p>
         </div>
     )
 }
